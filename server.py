2a1c56e2f087262ae27aa49a75b889ba45dd6ad8
John Allwine <john@pocketnc.com>
2021-10-22 16:14:05 +0000

    added server.py which doesn't cache index.html SOFT-745

---
+++ server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import http.server
+import socketserver
+
+class NoCachingIndexRequestHandler(http.server.SimpleHTTPRequestHandler):
+  def end_headers(self):
+    if self.path == "/index.html":
+      self.send_header("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate")
+      self.send_header("Pragma", "no-cache")
+      self.send_header("Expires", "0")
+    http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+  with socketserver.TCPServer(("", 80), NoCachingIndexRequestHandler) as server:
+    server.serve_forever()
--- startpenta-ui.sh
+++ startpenta-ui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /opt/penta-ui-builds/
-python3 -m http.server 80
+./server.py
